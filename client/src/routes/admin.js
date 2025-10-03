1f5c2cbb870c077767e4e9329ace92122bdba70f
Vyshnavi Arra <vyshnavi9arra@gmail.com>
2025-10-03 16:49:09 +0530

    Delete client directory

--- client/src/routes/admin.js
+++
@@ -1,7 +0,0 @@
-import api from "../services/api";
-
-export const fetchAdminStudentDashboard = (studentId) =>
-  api.get(`/admin/dashboard/student/${studentId}`).then((r) => r.data);
-
-export const fetchAdminStartupDashboard = (startupId) =>
-  api.get(`/admin/dashboard/startup/${startupId}`).then((r) => r.data);
